b97a82f3c108cf16038eebbe7500f516c298b51b
Kayumiy <qayum.mus@mail.ru>
2023-11-16 13:25:38 +0500

    SO scene test

---
+++ Football/Assets/Test/Scene workflow with SO/Level.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewLevel", menuName = "Scene Data/Level")]
+public class Level : GameScene
+{
+    //Settings specific to level only
+    [Header("Level specific")]
+    public int enemiesCount;
+
+
+
+}
